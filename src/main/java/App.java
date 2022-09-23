bc45185d965a2f57f2df368901c1810fff4c8593
Henrique-temponi <tanzenheavely@gmail.com>
2022-09-22 22:58:56 -0300

    Adiciona template para o front end (freemarker)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,32 @@
 import static spark.Spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
+
 public class App {
 	
 	public static void main(String[] args) {
 		
 		
-        staticFiles.location("/public");
+//        staticFiles.location("/");
         
-        get("/", (req, res) -> "<html><head><title>Ola mundo</title></head><body><h1>Metune</h1></body></html>");
+        get("/", (req, res) -> {
+        	Map<String, Object> map = new HashMap<>();
+        	map.put("Soforca", "tenta");
+        	return new FreeMarkerEngine().render(
+        		new ModelAndView(map, "template.ftl")
+        	);
+        });
+        
+        get("/test", (req, res) -> {
+        	Map<String, Object> map = new HashMap<>();
+        	map.put("test", "METUNE");
+        	return new FreeMarkerEngine().render(
+        		new ModelAndView(map, "/test/login.ftl")
+        	);
+        });
 	}
 }
