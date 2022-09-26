efad3bd0e64bd5815231e76942fcfb35be38c1fa
henrique-temponi <tanzenheavely@gmail.com>
2022-09-26 09:55:02 -0300

    Faz integracao com o front e backend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		
-//        staticFiles.location("/");
+        staticFiles.location("/public");
         
         get("/", (req, res) -> {
         	Map<String, Object> map = new HashMap<>();
