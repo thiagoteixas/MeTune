5d47f5dbce81798a73ea185f7719d102625c545c
Henrique-temponi <tanzenheavely@gmail.com>
2022-10-06 20:17:13 -0300

    Retira freemarker template do projeto

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,24 +13,6 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		
-        staticFiles.location("/public");
-        
-        get("/", (req, res) -> {
-        	Map<String, Object> map = new HashMap<>();
-        	map.put("Soforca", "tenta");
-        	return new FreeMarkerEngine().render(
-        		new ModelAndView(map, "template.ftl")
-        	);
-        });
-        
-        get("/test", (req, res) -> {
-        	Map<String, Object> map = new HashMap<>();
-        	map.put("test", "METUNE");
-        	return new FreeMarkerEngine().render(
-        		new ModelAndView(map, "/test/login.ftl")
-        	);
-        });
         
        get("/post", (req, res) -> {
     	   	res.type("aplication/json");
