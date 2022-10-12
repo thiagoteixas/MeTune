c305e6efe115e4b66a1c3da3f20d174ac7eea323
joaovitorgonzaga18 <54511843+joaovitorgonzaga18@users.noreply.github.com>
2022-10-12 20:02:16 -0300

    Add files via upload

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 
 
 import Header from './Components/Header.js';
+import Banner from './Components/Banner.js';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div className="banner">
+      <Banner></Banner>
+      </div>
     </div>
   );
 }
--- Front End/src/Components/Header.js
+++ Front End/src/Components/Header.js
@@ -13,7 +13,7 @@ import LogoMetune from '../img/metune.jpg'
 import Image from 'react-bootstrap/Image'
 import Figure from 'react-bootstrap/Figure';
 
-import { BsFillPersonFill } from "react-icons/bs";
+import { BsFillPersonFill, BsSearch, BsFillHeartFill } from "react-icons/bs";
 
 import '../App.css';
 
@@ -46,29 +46,10 @@ function Header () {
                 style={{ maxHeight: '100px' }}
                 navbarScroll
               >
-                <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
+              <BsFillHeartFill size={50} className="icon-header" color="red" />
+              <BsSearch size={50} className="icon-header" color="white" />
+              <BsFillPersonFill size={60} className="icon-header" color="white" />
               </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Quero escutar..."
-                  className="me-2"
-                  aria-label="Pesquisar"
-                />
-                <Button variant="light">Vamos Lá!</Button>
-              </Form>
-              <BsFillPersonFill />
             </Navbar.Collapse>
           </Container>
         </Navbar>
