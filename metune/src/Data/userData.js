5d219e38df1f1102da4c376ca0a62dce2574b0ac
thiagoteixas <thiagoteixas@gmail.com>
2022-10-12 23:35:33 -0300

    Feat: Adding model mocks

---
+++ metune/src/Data/userData.js
@@ -0,0 +1,34 @@
+const userList = [
+  {
+    id: '1',
+    email: 'joao@123.com.br',
+    username: 'Joaozao',
+    isPremium: true,
+  },
+  {
+    id: '2',
+    email: 'pedro@123.com.br',
+    username: 'Pedro123',
+    isPremium: true,
+  },
+  {
+    id: '3',
+    email: 'caio@123.com.br',
+    username: 'caioba',
+    isPremium: false,
+  },
+  {
+    id: '4',
+    email: 'carla@olas.com.br',
+    username: 'Carlota1221',
+    isPremium: false,
+  },
+  {
+    id: '5',
+    email: 'donageralda@gmail.com.br',
+    username: 'donaGege',
+    isPremium: false,
+  },
+];
+
+module.exports = userList;
