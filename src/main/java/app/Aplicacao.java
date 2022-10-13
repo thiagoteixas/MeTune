4d8647bdd3c7a61c6f32aa877dbbe26481812552
thiagoteixas <thiagoteixas@gmail.com>
2022-10-13 13:51:43 -0300

    Refactor: Changing routes

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -31,8 +31,9 @@ public class Aplicacao {
 //       		return map;
        });*/
 	  
-	  post("/Song", (request, response) -> SongService.insert(request, response));
-	  get("/Song/:id", (request, response) -> SongService.get(request, response));
-	  put("/Song/:id", (request, response) -> SongService.update(request, response));
+	  post("/song", (request, response) -> SongService.insert(request, response));
+	  get("/song/:id", (request, response) -> SongService.get(request, response));
+	  put("/song/update/:id", (request, response) -> SongService.update(request, response));
+	  get("/song/delete/:id", (request, response) -> SongService.delete(request, response));
 	} 
 }
