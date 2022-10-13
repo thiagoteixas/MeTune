e83a4afb8f489a0230060651dd9af4ab0109a8ba
EduardaNunis <87497387+EduardaNunis@users.noreply.github.com>
2022-10-13 20:36:02 -0300

    src/main/java

---
+++ App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import LoginPage from './Pages/LoginPage';
+import HomePage from './Pages/HomePage';
+import './Pages/LoginPage/style.css';
+
+function App() {
+  return (
+    <div className="app">
+        <LoginPage />
+    </div>
+  );
+}
+
+export default App;
